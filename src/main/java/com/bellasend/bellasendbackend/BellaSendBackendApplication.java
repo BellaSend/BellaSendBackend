357d1c5a85df22e422f445a4cf6a1df4bb388e73
Haileab-TG <haileab.t.g@gmail.com>
2023-10-19 22:51:47 -0500

    Initial Setup

---
+++ src/main/java/com/bellasend/bellasendbackend/BellaSendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bellasend.bellasendbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellaSendBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BellaSendBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bellasend/bellasendbackend/BellaSendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bellasend.bellasendbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellaSendBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
